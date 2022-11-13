3bf5d2dce412496e0d8ad8e37b27cc18877bca25
Hilmar Lapp <hlapp@drycafe.net>
2022-11-13 18:19:40 -0500

    Adds diagrams and data instructionss for Recursive SQL

---
+++ data/README.md
@@ -0,0 +1,9 @@
+## Datasets used in rendering
+
+### Recursive data structures in SQL
+
+-   The [lesson slides](../Lesson-06.qmd) use the [NCBI Taxonomy Database](https://www.ncbi.nlm.nih.gov/taxonomy) *"from a database dump downloaded from the NCBI FTP server on 2017-02-3, imported in a SQLite database table as a phylogenetic tree"* by R. Vos:
+
+    > Vos, Rutger (2017): NCBI taxonomy database files. figshare. Dataset. <https://doi.org/10.6084/m9.figshare.4620733.v1>
+
+    Download the file (ncbi-2017-02-03.db), then open in SQLite and apply the SQL (DDL) statements in [`ncbi-transform.sql`](ncbi-transform.sql).
---
+++ data/ncbi-transform.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Node1 (
+  ID INTEGER PRIMARY KEY,
+  Name TEXT,
+  Parent_ID INTEGER
+    REFERENCES Node1(ID),
+  "Left" INTEGER,
+  "RIGHT" INTEGER
+);
+
+INSERT INTO Node1 (ID, Name, Parent_ID, "Left", "Right")
+SELECT n.id, n.name, n.parent, n.left, n.right
+FROM   Node AS n, Node AS p
+WHERE  p.name = 'Eukaryota' 
+AND    n.left >= p.left and n.right <= p.right
+;
+
+ALTER TABLE Node RENAME TO Node_Original;
+ALTER TABLE Node1 RENAME TO Node;
+CREATE INDEX Node_Parent_Idx ON Node(Parent_ID);
+CREATE INDEX Node_Name_Idx ON Node(Name);
+CREATE INDEX Node_Left_Idx ON Node(Left);
+CREATE INDEX Node_Right_Idx ON Node(Right);
